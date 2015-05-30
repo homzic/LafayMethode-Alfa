0cfaa00c3b1483504d4af07367efec60cc7a6fca
Kris <krzysiek.serbin@gmail.com>
2015-05-30 19:14:07 +0200

    commit

--- app/build/generated/source/r/debug/com/kris/lm/R.java
+++ app/build/generated/source/r/debug/com/kris/lm/R.java
@@ -1557,7 +1557,7 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f090083;
+        public static final int action_settings=0x7f090085;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090016;
         public static final int beginning=0x7f090013;
@@ -1571,7 +1571,7 @@ containing a value of this type.
         public static final int counter=0x7f090068;
         public static final int cwDesc=0x7f090067;
         public static final int cwIcon=0x7f090062;
-        public static final int cwiczenia=0x7f090081;
+        public static final int cwiczenia=0x7f090083;
         public static final int decor_content_parent=0x7f09002f;
         public static final int default_activity_button=0x7f090024;
         public static final int dialog=0x7f09001b;
@@ -1619,13 +1619,14 @@ containing a value of this type.
         public static final int listText=0x7f090060;
         public static final int list_item=0x7f090025;
         public static final int list_slidermenu=0x7f09004e;
-        public static final int load_DB=0x7f090084;
+        public static final int load_DB=0x7f090086;
         public static final int middle=0x7f090015;
         public static final int never=0x7f090019;
         public static final int none=0x7f09000e;
         public static final int normal=0x7f09000a;
         public static final int progress_circular=0x7f090005;
         public static final int progress_horizontal=0x7f090006;
+        public static final int progressbar=0x7f090080;
         public static final int radio=0x7f09002b;
         public static final int radioActivity=0x7f09005a;
         public static final int radioFemale=0x7f090055;
@@ -1650,6 +1651,7 @@ containing a value of this type.
         public static final int showHome=0x7f090010;
         public static final int showTitle=0x7f090011;
         public static final int split_action_bar=0x7f090007;
+        public static final int start=0x7f090081;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000b;
         public static final int tableLayout=0x7f09004f;
@@ -1673,8 +1675,8 @@ containing a value of this type.
         public static final int txtWarmup=0x7f090075;
         public static final int up=0x7f090008;
         public static final int useLogo=0x7f090012;
-        public static final int user_body=0x7f090082;
-        public static final int user_data=0x7f090080;
+        public static final int user_body=0x7f090084;
+        public static final int user_data=0x7f090082;
         public static final int withText=0x7f09001a;
         public static final int wrap_content=0x7f09001d;
     }
--- app/src/main/java/com/kris/lm/Fragments/TrainingFragment.java
+++ app/src/main/java/com/kris/lm/Fragments/TrainingFragment.java
@@ -2,22 +2,75 @@ package com.kris.lm.Fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.kris.lm.R;
 
 public class TrainingFragment extends Fragment {
-	
-	public TrainingFragment(){}
-	
-	@Override
+    Button buttonStart;
+    ProgressBar progressBar;
+    TextView textCounter;
+    long odliczaj = 100000;
+    long interval = 1000;
+    private int mDisplaySeconds, mDisplayMinutes;
+
+    MyCountDownTimer myCountDownTimer;
+
+    public TrainingFragment() {
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
- 
+                             Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.fragment_training, container, false);
-         
+
+        buttonStart = (Button) rootView.findViewById(R.id.start);
+        progressBar = (ProgressBar) rootView.findViewById(R.id.progressbar);
+        textCounter = (TextView) rootView.findViewById(R.id.counter);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                progressBar.setProgress(100);
+                myCountDownTimer = new MyCountDownTimer(odliczaj, interval);
+                myCountDownTimer.start();
+
+            }
+        });
         return rootView;
     }
+
+
+    public class MyCountDownTimer extends CountDownTimer {
+
+        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            mDisplayMinutes = (int) ((millisUntilFinished / 1000) / 60);
+            mDisplaySeconds = (int) ((millisUntilFinished / 1000) % 60);
+            int progress = (int) (millisUntilFinished / 1000);
+            progressBar.setProgress(progress);
+            textCounter.setText(String.valueOf(mDisplayMinutes)  +":" + String.valueOf(mDisplaySeconds) + "\nProgres: " + String.valueOf(progress));
+
+        }
+
+        @Override
+        public void onFinish() {
+            textCounter.setText("Finished");
+            progressBar.setProgress(0);
+        }
+
+    }
+
 }
